f35138e597b5129591d93839762ed4eea726aa5a
Trina <trina.sikdar.2101@gmail.com>
2024-07-21 14:36:06 +0530

    1

--- src/BookList.js
+++ src/BookList.js
@@ -1,37 +1,39 @@
 import './BookList.css';
-
-const firstBook = {
-	author: 'Saksham Garg',
-	title: 'Samsara: Enter The Valley Of The Gods',
-	img: './images/book1.jpg'
-};
-const secondBook = {
-	author: 'J.K.Rowling',
-	title: "Harry Potter and the Philosopher's Stone",
-	img: 'https://images-eu.ssl-images-amazon.com/images/I/81m9fP+LIPL._UX300__PJku-sticker-v8,TopRight,0,-50_AC_UL600_SR600,400_.jpg'
-};
+const books = [
+	{
+		id: 1,
+		author: 'Saksham Garg',
+		title: 'Samsara: Enter The Valley Of The Gods',
+		img: './images/book1.jpg'
+	},
+	{
+		id: 2,
+		author: 'J.K.Rowling',
+		title: "Harry Potter and the Philosopher's Stone",
+		img: 'https://images-eu.ssl-images-amazon.com/images/I/81m9fP+LIPL._UX300__PJku-sticker-v8,TopRight,0,-50_AC_UL600_SR600,400_.jpg'
+	}
+];
 
 const BookList = () => {
 	return (
 		<section className='bookList'>
-			<Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
-				<p>Anim consectetur sunt do consectetur fugiat velit.</p>
-				<button>Click Me!</button>
-			</Book>
-			<Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
+			{books.map((book, index) => {
+				const { img, title, author, id } = book;
+				console.log(book, index);
+				return (
+					<Book key={index} img={img} title={title} author={author} />
+				)
+			})}
 		</section>
 	);
 };
 
-const Book = ({ img, title, author, children }) => {
-	// console.log(props)
-	// const { img, title, author, children } = props;
+const Book = ({ img, title, author }) => {
 	return (
 		<article className='book'>
 			<img src={img} alt={title} />
 			<h2>{title}</h2>
 			<h4>{author}</h4>
-			{children}
 		</article>
 	)
 };
